a6913fcc2d74f475b4852f4383a72b7ecc6d83b6
Xy718 <869839000@qq.com>
2020-03-06 15:48:44 +0800

    1

---
+++ src/main/java/xyz/xy718/poster/XyDashbosrdPosterPlugin.java
@@ -0,0 +1,15 @@
+package xyz.xy718.poster;
+
+import org.spongepowered.api.plugin.Plugin;
+
+import xyz.xy718.getdrops.GetDropsPlugin;
+
+@Plugin(
+id = XyDashbosrdPosterPlugin.PLUGIN_ID
+, name = XyDashbosrdPosterPlugin.NAME
+, version = XyDashbosrdPosterPlugin.VERSION
+, description = XyDashbosrdPosterPlugin.DESCRIPTION
+)
+public class XyDashbosrdPosterPlugin {
+
+}
